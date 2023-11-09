3b0139eb14aec1f040359a4ca27167eeabfcf144
Djamet Code <kampretsiji1@gmail.com>
2023-11-09 19:05:36 +0700

    djamet

--- backend/prod/app.js
+++ backend/prod/app.js
@@ -16,8 +16,17 @@ require("dotenv/config");
 const cloudinary_1 = require("cloudinary");
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
 const app = (0, express_1.default)();
 const port = 3000 || process.env.PORT;
+const configSchema = zod_1.z.object({
+    MONGO_URL: zod_1.z.string(),
+    API_KEY: zod_1.z.string(),
+    API_SECRET: zod_1.z.string(),
+    CLOUD_NAME: zod_1.z.string(),
+    JWT_SECRET: zod_1.z.string(),
+    JWT_TIMES: zod_1.z.string(),
+});
 //database connect
 const connectDB_1 = require("./db/connectDB");
 const authRoute_1 = require("./route/authRoute");
@@ -25,13 +34,15 @@ const postRoute_1 = require("./route/postRoute");
 cloudinary_1.v2.config({
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
-    cloud_name: process.env.CLOUD_NAME
+    cloud_name: process.env.CLOUD_NAME,
 });
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use('/api/v17/no-life/auth', authRoute_1.authRouter);
-app.use('/api/v17/no-life/post', postRoute_1.postRouter);
+app.use("/api/v17/no-life/auth", authRoute_1.authRouter);
+app.use("/api/v17/no-life/post", postRoute_1.postRouter);
+const mongo_url = configSchema.parse(process.env);
+console.log(mongo_url);
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield (0, connectDB_1.connectDB)(process.env.MONGO_URL);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,34 +1,50 @@
-import 'dotenv/config'
-import { v2 as cloudinary } from 'cloudinary'
-import cors from 'cors'
-import express from 'express'
+import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
+import express from "express";
+import { z } from "zod";
 const app = express();
-const port = 3000 || process.env.PORT
+const port = 3000 || process.env.PORT;
+
+const configSchema = z.object({
+  MONGO_URL: z.string(),
+  API_KEY: z.string(),
+  API_SECRET: z.string(),
+  CLOUD_NAME: z.string(),
+  JWT_SECRET: z.string(),
+  JWT_TIMES: z.string(),
+});
 
 //database connect
-import { connectDB } from './db/connectDB';
-import { authRouter } from './route/authRoute';
-import { postRouter } from './route/postRoute';
+import { connectDB } from "./db/connectDB";
+import { authRouter } from "./route/authRoute";
+import { postRouter } from "./route/postRoute";
 
 cloudinary.config({
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-    cloud_name: process.env.CLOUD_NAME
-})
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+});
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use('/api/v17/no-life/auth', authRouter)
-app.use('/api/v17/no-life/post', postRouter)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/v17/no-life/auth", authRouter);
+app.use("/api/v17/no-life/post", postRouter);
+
+const mongo_url = configSchema.parse(process.env);
+console.log(mongo_url);
 
 const startServer = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL)
-        app.listen(port, () => console.log("Sever running"))
-    } catch (error) {
-        console.log(error)
+  try {
+    if (!mongo_url) {
+      console.log("Validate error");
     }
-}
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log("Sever running"));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-startServer()
+startServer();
