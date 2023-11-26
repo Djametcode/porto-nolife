7f810b851dfbc5020c5cde582ae0aa659da48995
djamet <kampretsiji1@gmail.com>
2023-11-27 00:44:54 +0700

    changes

--- frontend/no-life-porto/src/app/reels/layout.tsx
+++ frontend/no-life-porto/src/app/reels/layout.tsx
@@ -10,17 +10,27 @@ interface Iuser {
   username: string;
 }
 
+interface IResponse {
+  msg: string;
+  user: {
+    username: string;
+    avatar: string;
+  };
+}
+
 export default function ReelsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [user, setUser] = useState<Iuser>();
+  const [user, setUser] = useState<Iuser[]>([]);
   const getUser = async () => {
     try {
-      const response = await getCurrentUser();
+      const response = (await getCurrentUser()) as IResponse;
       console.log(response);
-      setUser(response.user);
+      setUser([
+        { avatar: response.user.avatar, username: response.user.username },
+      ]);
     } catch (error) {
       console.log(error);
     }
@@ -33,7 +43,9 @@ export default function ReelsLayout({
   return (
     <div className=" h-screen bg-black text-white font-figtree">
       {children}
-      <NavbarComponent avatar={user?.avatar} />
+      {user.map((item) => {
+        return <NavbarComponent key={item.avatar} avatar={item.avatar} />;
+      })}
     </div>
   );
 }
